36435d65c4ce03a98d98f6a0cfd935ef06cb7381
Guilherme <guilherme.gl1997@gmail.com>
2021-01-25 15:50:35 -0300

    feat: project interface

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import db from "../db.json";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+`;
+
+const theme = db.theme;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import QuizBackground from "../src/components/QuizBackground";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+
+const QuizContainer = styled.div`
+  width: 100%;
+  height: calc(100vh - 45px);
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    height: 100vh;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Dark Souls</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+
+            <p>Lorem ipsum sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/gui-leandro/darksouls-quiz" />
+    </QuizBackground>
+  );
+}
